156c2cbf2f8d75cb2aa28ccb2ada068c827cc5d7
Jose Nunez <josenunez@Joses-MacBook-Air-3.local>
2024-10-05 14:57:00 -0500

    Hello

--- nasa-website/src/map.js
+++ nasa-website/src/map.js
@@ -3,17 +3,38 @@ import { MapContainer, TileLayer } from "react-leaflet";
 
 function Map() {
   return (
-    <div style={{ width: "500px", height: "500px", border: "1px solid black" }}>
-      <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        style={{ width: "100%", height: "100%" }}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh", // makes the parent div take up the full viewport height
+      }}
+    >
+      {/* Header */}
+      <h1 style={{ marginBottom: "20px" }}>Map View</h1>
+
+      {/* Map Container */}
+      <div
+        style={{
+          width: "800px",
+          height: "500px",
+          border: "1px solid black",
+          overflow: "hidden",
+        }}
       >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-        />
-      </MapContainer>
+        <MapContainer
+          center={[38, -0.09]}
+          zoom={5}
+          style={{ width: "100%", height: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+          />
+        </MapContainer>
+      </div>
     </div>
   );
 }
